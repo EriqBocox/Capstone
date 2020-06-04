4b4de5ddc4a912c70940227413c3cf24d31e30d8
eriq <erbocox@aol.com>
2020-06-04 16:56:39 -0400

    login/register forms working

---
+++ Capstone/src/main/java/com/weather/Capstone/models/data/UserRepository.java
@@ -0,0 +1,10 @@
+package com.weather.Capstone.models.data;
+
+import com.weather.Capstone.models.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    User findByUsername(String username);
+
+}
